8cbdb398e4780057b55f975ac9bcb02c2f81cb78
Johnny Chen <mcjohnnychen@gmail.com>
2017-11-30 14:22:41 -0800

    Add NowPlaying section

--- components/App.jsx
+++ components/App.jsx
@@ -3,6 +3,7 @@ import Paper from 'material-ui/Paper';
 import AppBar from 'material-ui/AppBar';
 import SearchBox from '../containers/SearchBox';
 import MovieDetail from '../containers/MovieDetail';
+import NowPlaying from '../containers/NowPlaying';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         title="Cliff Movie DB"
         iconElementLeft={<img src="https://s3.amazonaws.com/tt-public-assets/Cliff_icon.png" alt="Logo" />}
       />
+      <NowPlaying />
       <SearchBox />
       <MovieDetail />
     </Paper>
