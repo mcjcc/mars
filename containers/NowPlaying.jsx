89bbdd68e95a60433eea073d04774c2cda5daa75
Johnny Chen <mcjohnnychen@gmail.com>
2017-11-30 18:58:06 -0800

    Refactor nowplaying functions and added css to show movie tiles as clickable

--- containers/NowPlaying.jsx
+++ containers/NowPlaying.jsx
@@ -5,19 +5,37 @@ import { bindActionCreators } from 'redux';
 import Paper from 'material-ui/Paper';
 import axios from 'axios';
 import MovieList from '../components/MovieList';
+import { fetchMovie1, fetchMovie2 } from '../actions/MovieAction';
+
 
 class NowPlaying extends Component {
   constructor() {
     super();
     this.state = {
-      newReleases: []
+      nowPlaying: []
     }
 
-    this.fetchNewReleases = this.fetchNewReleases.bind(this);
+    this.fetchNowPlaying = this.fetchNowPlaying.bind(this);
+    this.fetchPrimaryMovie = this.fetchPrimaryMovie.bind(this);
   }
 
-  fetchNewReleases() {
+  fetchPrimaryMovie(id) {
+    this.setState({ primaryMovieList: [] });
+    this.props.fetchMovie1(id);
+  }
 
+  fetchNowPlaying() {
+    console.log('fetch now playing');
+    axios.get('/fetchNowPlaying')
+    .then((response) => {
+      console.log('fetchnowplaying then', response);
+      this.setState({ nowPlaying: response.data.results });
+    });
+  }
+
+  componentDidMount() {
+    console.log('will mount!');
+    this.fetchNowPlaying();
   }
 
   render() {
@@ -26,8 +44,8 @@ class NowPlaying extends Component {
       <div>
         Now Playing
         <MovieList
-          movies={this.state.newReleases}
-          fetchMovie={this.fetchNewReleases}
+          movies={this.state.nowPlaying}
+          fetchMovie={this.fetchPrimaryMovie}
         />
       </div>
     );
@@ -35,4 +53,13 @@ class NowPlaying extends Component {
   }
 }
 
-export default NowPlaying;
+function mapStateToProps({ primaryMovie, secondaryMovie }) {
+  return { primaryMovie, secondaryMovie };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchMovie1, fetchMovie2 }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NowPlaying);
+// export default NowPlaying;
