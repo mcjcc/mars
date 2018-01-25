085806a3a15edf9d56967d5c0a3716001d842df5
Johnny Chen <mcjohnnychen@gmail.com>
2018-01-24 19:30:04 -0800

    Remove user profile actions

--- actions/MovieAction.js
+++ actions/MovieAction.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 export const FETCH_MOVIE1 = 'FETCH_MOVIE1';
 export const FETCH_MOVIE2 = 'FETCH_MOVIE2';
-export const FETCH_PROFILE = 'FETCH_PROFILE';
-export const CREATE_PROFILE = 'CREATE_PROFILE';
-export const LOGOUT = 'LOGOUT';
 
 export function fetchMovie1(id) {
   const request = axios.get(`/movie/${id}`);
@@ -22,28 +19,4 @@ export function fetchMovie2(id) {
     type: FETCH_MOVIE2,
     payload: request,
   };
-}
-
-export function fetchProfile(username, password) {
-  const request = axios.get(`/profile/${username}/${password}`);
-
-  return {
-    type: FETCH_PROFILE,
-    payload: request,
-  };
-}
-
-export function createProfile(username, password) {
-  const request = axios.post(`/profile`, {username, password});
-
-  return {
-    type: CREATE_PROFILE,
-    payload: request,
-  }
-}
-
-export function logout() {
-  return {
-    type: LOGOUT,
-  }
 }
